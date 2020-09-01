24b76e41b3ecc7a8e497d75c1a2dd7414e8d2a85
Sergio Martinez <sergimar@stud.ntnu.no>
2020-09-01 20:05:08 +0200

    xd

--- src/main/java/oving3/BinarySearchTree.java
+++ src/main/java/oving3/BinarySearchTree.java
@@ -1,13 +1,31 @@
 package oving3;
 
+import javax.management.remote.rmi._RMIConnection_Stub;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class BinarySearchTree {
     Node root;
 
     public BinarySearchTree(String value) {
+        if (!value.contains(",")  && !value.contains(" ")) {
         this.root = new Node(value);
+        }
+        else if (value.contains(",")) {
+            String [] notes = value.split(",");
+            this.root = new Node(notes[0]);
+            for (int i = 1; i < notes.length; i++) {
+                add(notes[i]);
+            }
+        }else if (value.contains(" ")) {
+            String [] notes = value.split(" ");
+            this.root = new Node(notes[0]);
+            for (int i = 1; i < notes.length; i++) {
+                add(notes[i]);
+            }
+        }
+
     }
 
     public void add(String value) {
@@ -35,15 +53,28 @@ public class BinarySearchTree {
         queue.add(root);
         LinkedList<String> words = new LinkedList<String>();
         words.add(root.getValue());
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty() && queue.size()<1024) {
             Node currentNote = queue.remove();
-            if (currentNote.getLeft() != null) {
-                queue.add(currentNote.getLeft());
-                words.add(currentNote.getLeft().getValue());
-            }
-            if (currentNote.getRight() != null) {
-                queue.add(currentNote.getRight());
-                words.add(currentNote.getRight().getValue());
+            if (!currentNote.getValue().equals("_")) {
+                if (currentNote.getLeft() != null) {
+                    queue.add(currentNote.getLeft());
+                    words.add(currentNote.getLeft().getValue());
+                }else {
+                    words.add("_");
+                    queue.add(new Node("_"));
+                }
+                if (currentNote.getRight() != null) {
+                    queue.add(currentNote.getRight());
+                    words.add(currentNote.getRight().getValue());
+                }else {
+                    words.add("_");
+                    queue.add(new Node("_"));
+                }
+            }else {
+                words.add("_");
+                words.add("_");
+                queue.add(new Node("_"));
+                queue.add(new Node("_"));
             }
         }
         return words;
@@ -52,20 +83,44 @@ public class BinarySearchTree {
     public void printTree() {
         LinkedList<String> words = getWords();
         int counter = 1;
-        int lines = (int) (Math.log(words.size()) / Math.log(2))+1;
-        while (counter <= lines) {
+        while (words.size() != 0) {
+
             for (int i = 0; i < counter; i++) {
-                System.out.print(words.remove()+" ");
+
+                StringBuilder builder = new StringBuilder();
+                if (words.size() != 0) {
+
+                    for (int j = 0; j < (64/counter)-(words.get(0).length()/2); j++) {
+                        builder.append(" ");
+                    }
+                }
+                System.out.print(builder.toString());
+
+                if (words.size() != 0) {
+                    if (words.get(0) == "_" ) {
+                        System.out.print("");
+                        words.remove();
+                    }
+                    else {
+                        System.out.print(words.remove());
+                    }
+                }
+                System.out.print(builder.toString());
             }
+            System.out.println();
+            System.out.println();
             System.out.println();
             counter *= 2;
         }
     }
 
     public static void main(String[] args) {
-        BinarySearchTree binarySearchTree = new BinarySearchTree("test");
-        binarySearchTree.add("voon");
-        binarySearchTree.add("bark");
+        Scanner scanner = new Scanner(System.in);
+        BinarySearchTree binarySearchTree = new BinarySearchTree(scanner.nextLine());
         binarySearchTree.printTree();
+        while (true) {
+            binarySearchTree.add(scanner.nextLine());
+            binarySearchTree.printTree();
+        }
     }
 }
