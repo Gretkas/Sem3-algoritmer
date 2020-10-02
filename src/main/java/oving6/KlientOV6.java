215c1eb5e109dbdbf30f75d9cbf44f7d171879fe
Robin V <robvold98@gmail.com>
2020-10-02 14:44:09 +0200

    leveringsver

--- src/main/java/oving6/KlientOV6.java
+++ src/main/java/oving6/KlientOV6.java
@@ -20,14 +20,7 @@ public class KlientOV6 {
         BufferedReader br = new BufferedReader(new FileReader(new File(path)));
         Graph g = new Graph();
         g.newGraph(br);
-
-        /*for (int i = 0; i < g.getPriorityQueue().length; i++) {
-            System.out.println(g.getPriorityQueue()[i].toString());
-            for (Edge e = g.getPriorityQueue()[i].getEdge();e!=null;e = e.getNextEdge()) {
-                System.out.println(e.toString());
-            }
-        }
-*/
+        
 
         g.dijkstra(1);
         g.sortResult();
