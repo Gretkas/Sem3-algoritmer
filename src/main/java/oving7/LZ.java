0b3bbb16a6fb465a47227fe2608ae53669d22d39
Robin V <robvold98@gmail.com>
2020-10-21 11:05:36 +0200

    fiksa 4 byte chars i LZ, men fortsatt problem med PDF filer

--- src/main/java/oving7/LZ.java
+++ src/main/java/oving7/LZ.java
@@ -1,14 +1,7 @@
 package oving7;
 
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 public class LZ {
     private byte[] bFilArr;
@@ -16,8 +9,8 @@ public class LZ {
 
     public static void main(String[] args) throws IOException {
         LZ lz = new LZ();
-        lz.compress("C:\\Users\\robvo\\Desktop\\resources\\oving7\\testFil.txt");
-        lz.decompress("C:\\Users\\robvo\\Desktop\\resources\\oving7\\testFilLZ.txt");
+        lz.compress("C:\\Users\\robvo\\Desktop\\resources\\oving7\\diverse.lyx");
+        lz.decompress("C:\\Users\\robvo\\Desktop\\resources\\oving7\\diverseLZ.lyx");
     }
 
     public void compress(String inputPath) throws IOException {
@@ -75,14 +68,28 @@ public class LZ {
                 short index = (short)(((tempShort[0] & 0xFF) << 8) | (tempShort[1] & 0xFF));    //her kan det være feil!!!
                 StringBuilder currentSequence = new StringBuilder();
 
-                if(index == 8162){
-                    System.out.println("inni if et sted");
-                }
 
-
-                for (int i = index; i <index + currentBlockLength-1; i++) {
+                for (int i = index;(i <index + currentBlockLength)/* && (i< sequences.length())*/; i++) { //var -1 før
                     currentSequence.append(sequences.charAt(i));
                 }
+
+
+                //start av test
+                /*if(currentSequence.charAt(currentSequence.length()) > ){
+                    currentChar = new byte[1];
+                    currentChar[0] = currentByte;
+                    currentSequence.append((char)currentChar[0]);
+                }else {
+
+                    String binaryString = Integer.toBinaryString(currentByte & 0xff);
+                    for (int i = 1; i < 4; i++) {
+                        if(binaryString.charAt(i) == 49) charLength++;
+                        else break;
+                    }
+                }*/
+                //slutt av test
+
+                //går tom for characerts
 
 
                 currentByte = bFilArr[byteIndex++];
@@ -98,6 +105,7 @@ public class LZ {
                         if(binaryString.charAt(i) == 49) charLength++;
                         else break;
                     }
+
                     int tempIndex = 1;
                     currentChar = new byte[charLength];
                     currentChar[0] = currentByte;
@@ -130,6 +138,7 @@ public class LZ {
                             if (binaryString.charAt(j) == 49) charLength++;
                             else break;
                         }
+
                         int tempIndex = 1;
                         currentChar = new byte[charLength];
                         currentChar[0] = currentByte;
@@ -167,10 +176,8 @@ public class LZ {
         bFilArr = new byte[innfil.available()];
         innfil.readFully(bFilArr, 0, innfil.available());
         int byteIndex = 0;
-        boolean lastUncompressed = false;
-        StringBuffer uncompressedOut = new StringBuffer();
-
-
+        boolean lastUncompressed = true;
+        StringBuilder uncompressedOut = new StringBuilder();
 
 
         while (byteIndex < bFilArr.length){
@@ -204,27 +211,41 @@ public class LZ {
             indexDos = sequences.indexOf(currentSequence.toString());
 
             if(indexDos < 0 || byteIndex == bFilArr.length || currentSequence.length() > 126){
-                if (currentSequence.length() > 2) {
+                int length = handleLength(currentSequence);
+                if (length > 2) {
                     lastUncompressed = false;
                 }
                 sequences.append(currentSequence.toString());
                 if (!lastUncompressed) {
                     writeUncompressed(utfil, uncompressedOut);
-                    uncompressedOut = new StringBuffer();
+                    uncompressedOut = new StringBuilder();
                 }
-                if(currentSequence.length() > 2){
+                if(length > 2){
+
+
+                    /*int sequenceLength = currentSequence.length();
+                    System.out.println(Integer.toBinaryString(currentSequence.charAt(currentSequence.length()-1) & 0xff));
+                    if(Integer.toBinaryString(currentSequence.charAt(currentSequence.length()-2)).charAt(0) == '1' && Integer.toBinaryString(currentSequence.charAt(currentSequence.length()-2)).length() == 8){   //vi har den for å fikse en feil med blokklengden og tegn på 4 bytes
+                        System.out.println("if kjører");
+                        byte x = (byte) ((byte) currentSequence.charAt(currentSequence.length()-2) & 0xff);
+                        String bitStr = Integer.toBinaryString(x);
+                        int xLength = 0;
+                        for (int i = 0; i < 4; i++) {
+                            if (bitStr.charAt(i) == 49) xLength++;
+                        }
+                        if(xLength == 4) sequenceLength--;
+                    }*/
+
+
+                    String strCurrentCharLength = new String(currentChar);
+                    int sequenceLength = currentSequence.length()-strCurrentCharLength.length();
 
                     currentOutputBlock = new byte[3+charLength];
-
-                    currentOutputBlock[0] = (byte) (currentSequence.length()  & 0xff);
+                    currentOutputBlock[0] = (byte) (sequenceLength & 0xff); //her gjorde vi en forandring. currentSequence.length
                     currentOutputBlock[1] = (byte) (prevIndex>>8);
                     currentOutputBlock[2] = (byte) (prevIndex & 0b11111111);
                     if(charLength == 1){
                         currentOutputBlock[3] = (byte) (currentChar[0] & 0xff);
-                    }
-
-                    if(currentOutputBlock[1] == 31 && currentOutputBlock[2] == -30){
-                        System.out.println("inni comp");
                     }
 
                     else{
@@ -232,7 +253,6 @@ public class LZ {
                             currentOutputBlock[i] = currentChar[i-3];
                         }
                     }
-
 
                     utfil.write(currentOutputBlock);
                 }else {
@@ -254,18 +274,50 @@ public class LZ {
         utfil.close();
     }
 
-    private void writeUncompressed(DataOutputStream utfil, StringBuffer uncompressedOut) throws IOException {
+
+    //fin en bedre måte å gjøre dette på dersom mulig
+    private int handleLength(StringBuilder currentSequence) {
+        int length = currentSequence.length();
+        int charLength = 1;
+        String currentChar = "";
+        byte[] bytes = currentSequence.toString().getBytes();
+
+
+
+        for (int i = 0; i < bytes.length; i++) {
+            currentChar = Integer.toBinaryString(bytes[i] & 0xff);
+            if(currentChar.charAt(0) != '0' && currentChar.length() == 8){
+
+
+                for (int j = 1; j < 4; j++) {
+                    if(currentChar.charAt(j) == 49)charLength++;
+                    else break;
+                }
+                if (charLength == 4){
+                    length--;
+
+                }
+                i+=charLength-1;
+                charLength = 1;
+            }
+
+        }
+        return length;
+    }
+
+    private void writeUncompressed(DataOutputStream utfil, StringBuilder uncompressedOut) throws IOException {
+        int length = handleLength(uncompressedOut);
         byte[] currentOutputBlock;
         byte[] currentSequenceBytes = uncompressedOut.toString().getBytes();
         currentOutputBlock = new byte[currentSequenceBytes.length+1];
-        currentOutputBlock[0] = (byte) (-uncompressedOut.length() & 0xff);
+        currentOutputBlock[0] = (byte) (-length & 0xff);
         for (int i = 0; i < currentSequenceBytes.length; i++) {
             currentOutputBlock[i+1] = currentSequenceBytes[i];
         }
         utfil.write(currentOutputBlock);
     }
 
-    
+
 
     private void trimList(){
         sequences.delete(0, sequences.length() - (1 << 15));
