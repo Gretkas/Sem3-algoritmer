b2ccaecdc0b87deb385345353c69d9b84fd90d40
Robin V <robvold98@gmail.com>
2020-10-21 13:59:47 +0200

    refactored LZ decomp

--- src/main/java/oving7/LZ.java
+++ src/main/java/oving7/LZ.java
@@ -1,14 +1,11 @@
 package oving7;
 
 import java.io.*;
-import java.util.Arrays;
 
 /**
  * Only works with UTF-8 encoded files!
  */
 public class LZ {
-    private byte[] bFilArr;
-    private StringBuffer sequences = new StringBuffer();
 
     /**
      * The entry point of application.
@@ -19,13 +16,14 @@ public class LZ {
     public static void main(String[] args) throws IOException {
         LZ lz = new LZ();
         lz.compress("C:\\Users\\robvo\\Desktop\\resources\\oving7\\fusk.txt");
-        lz.decompress("C:\\Users\\robvo\\Desktop\\resources\\oving7\\fuskLZ.txt");
+        lz.decompressFromFile("C:\\Users\\robvo\\Desktop\\resources\\oving7\\fuskLZ.txt");
     }
 
 
     /**
      * Compress.
      * Helper method for compress. Handles the output path if only running LZ and not huffmann
+     *
      * @param inputPath the input path
      * @throws IOException the io exception
      */
@@ -47,10 +45,11 @@ public class LZ {
     /**
      * Decompress.
      * Helper method for deCompress. Handles the output path if only running LZ and not huffmann
+     *
      * @param inputPath the input path
      * @throws IOException the io exception
      */
-    public void decompress(String inputPath) throws IOException {
+    public void decompressFromFile(String inputPath) throws IOException {
         StringBuilder outputPath = new StringBuilder();
         if (inputPath.contains(".")) {
             String[] arr = inputPath.split("\\.");
@@ -62,110 +61,119 @@ public class LZ {
         }else {
             outputPath.append(inputPath).append("Decomp");
         }
-        decompress(inputPath, outputPath.toString());
+        DataInputStream innfil = new DataInputStream(new BufferedInputStream(new FileInputStream(inputPath)));
+
+        byte[] bFilArr = new byte[innfil.available()];
+        innfil.readFully(bFilArr, 0, innfil.available());
+        decompress(bFilArr,outputPath.toString());
+        innfil.close();
     }
+
 
     /**
      * Decompress.
      *
-     * @param compressedFile the compressed file
-     * @param output         the output
+     * @param bFilArr the file as a byte array
+     * @param outputPath  the outputPath
      * @throws IOException the io exception
      */
-    public void decompress(String compressedFile, String output) throws IOException {
-        sequences = new StringBuffer();
-        DataInputStream innfil = new DataInputStream(new BufferedInputStream(new FileInputStream(compressedFile)));
-        DataOutputStream utfil = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(output)));
-        byte[] currentOutputBlock;
-
-        bFilArr = new byte[innfil.available()];
-        innfil.readFully(bFilArr, 0, innfil.available());
-        int byteIndex = 0;
+    public void decompress(byte[] bFilArr, String outputPath) throws IOException {
+        StringBuffer sequences = new StringBuffer();    //this is the sliding window
+        DataOutputStream utfil = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(outputPath)));
+        int byteIndex = 0;  //Index in the file-array
 
 
         while (byteIndex < bFilArr.length) {
-            int currentBlockLength = bFilArr[byteIndex++];
-            byte currentByte;
-            byte[] currentChar;
-            int charLength = 1;
+            int currentBlockLength = bFilArr[byteIndex++];  //if positive data is compressed, if not data is uncompressed;
 
-            if(currentBlockLength > 0){
-                byte[] tempShort = {bFilArr[byteIndex++],bFilArr[byteIndex++]};
-                short index = (short)(((tempShort[0] & 0xFF) << 8) | (tempShort[1] & 0xFF));
-                StringBuilder currentSequence = new StringBuilder();
-
-                for (int i = index;(i <index + currentBlockLength); i++) {
-                    currentSequence.append(sequences.charAt(i));
-                }
-
-                currentByte = bFilArr[byteIndex++];
-
-                if(currentByte >= 0){
-                    currentChar = new byte[1];
-                    currentChar[0] = currentByte;
-                    currentSequence.append((char)currentChar[0]);
-                }else {
-
-                    String binaryString = Integer.toBinaryString(currentByte & 0xff);
-                    for (int i = 1; i < 4; i++) {
-                        if(binaryString.charAt(i) == 49) charLength++;
-                        else break;
-                    }
-
-                    int tempIndex = 1;
-                    currentChar = new byte[charLength];
-                    currentChar[0] = currentByte;
-                    while(tempIndex < charLength){
-                        currentChar[tempIndex++] = bFilArr[byteIndex++];
-                    }
-                    currentSequence.append(new String(currentChar));
-                }
-
-                currentOutputBlock = currentSequence.toString().getBytes();
-                sequences.append(currentSequence.toString());
+            if(currentBlockLength > 0){ //This if handles compressed data
+                byteIndex = handleCompressedData(byteIndex,bFilArr,sequences,currentBlockLength,utfil);
             }
-            else{
-                int length = Math.abs(currentBlockLength);
-                currentOutputBlock = new byte[length];
-
-                StringBuilder currentSequence = new StringBuilder();
-
-                for (int i = 0; i < length; i++) {
-                    charLength = 1;
-                    currentByte = bFilArr[byteIndex++];
-                    if(currentByte >= 0) {
-
-                        currentOutputBlock[i] = currentByte;
-                        currentSequence.append((char)currentOutputBlock[i]);
-                    }
-                    else {
-                        String binaryString = Integer.toBinaryString(currentByte & 0xff);
-                        for (int j = 1; j < 4; j++) {
-                            if (binaryString.charAt(j) == 49) charLength++;
-                            else break;
-                        }
-
-                        int tempIndex = 1;
-                        currentChar = new byte[charLength];
-                        currentChar[0] = currentByte;
-                        while (tempIndex < charLength) {
-                            currentChar[tempIndex++] = bFilArr[byteIndex++];
-                        }
-
-                        currentSequence.append(new String(currentChar));
-                    }
-                }
-                currentOutputBlock = currentSequence.toString().getBytes();
-                sequences.append(currentSequence.toString());
+            else{ //This block handles uncompressed data
+                byteIndex = handleUncompressedData(byteIndex,bFilArr,sequences,currentBlockLength,utfil);
             }
 
-
-            utfil.write(currentOutputBlock);
-            if(sequences.length() > 1<<15) trimList();
+            //if the sliding window is over its max size
+            if(sequences.length() >= 1<<15) trimList(sequences);
         }
-        innfil.close();
         utfil.flush();
         utfil.close();
+    }
+
+
+
+
+    private int handleUncompressedData(int byteIndex, byte[] bFilArr, StringBuffer sequences, int currentBlockLength, DataOutputStream utfil) throws IOException {
+        int length = Math.abs(currentBlockLength);  //the length of the current block
+        StringBuilder currentSequence = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            byte currentByte = bFilArr[byteIndex++];
+            if(currentByte >= 0) {
+                currentSequence.append((char)currentByte);
+            }
+            else {
+                byteIndex = handleSpecialChar(bFilArr,byteIndex, currentByte, currentSequence);
+            }
+        }
+
+        sequences.append(currentSequence.toString());
+        utfil.write(currentSequence.toString().getBytes());
+
+        return byteIndex;
+    }
+
+
+
+
+    private int handleCompressedData(int byteIndex, byte[] bFilArr, StringBuffer sequences, int currentBlockLength, DataOutputStream utfil) throws IOException {
+        StringBuilder currentSequence = new StringBuilder();
+        byte[] tempShort = {bFilArr[byteIndex++],bFilArr[byteIndex++]};
+        short index = (short)(((tempShort[0] & 0xFF) << 8) | (tempShort[1] & 0xFF)); //What is the index of our compressed data in the sliding window
+
+
+        for (int i = index;(i <index + currentBlockLength); i++) {  //collect our sequence from the sliding window
+            currentSequence.append(sequences.charAt(i));
+        }
+
+        //handles the last character of the sequence
+        byte currentByte = bFilArr[byteIndex++];
+        byte[] currentChar;
+
+        if(currentByte >= 0){ //if character is one byte
+            currentChar = new byte[1];
+            currentChar[0] = currentByte;
+            currentSequence.append((char)currentChar[0]);
+        }else { //if character is more than one byte
+            byteIndex = handleSpecialChar(bFilArr, byteIndex, currentByte,currentSequence);
+        }
+
+        //adds the sequence to the output, and appends it to the sliding window.
+        sequences.append(currentSequence.toString());
+        utfil.write(currentSequence.toString().getBytes());
+
+        return byteIndex;
+    }
+
+
+    private int handleSpecialChar(byte[] bFilArr,int byteIndex, byte currentByte, StringBuilder currentSequence) {
+        byte[] currentChar;
+        int charLength = 1;
+
+        String binaryString = Integer.toBinaryString(currentByte & 0xff);
+        for (int i = 1; i < 4; i++) {
+            if(binaryString.charAt(i) == 49) charLength++;
+            else break;
+        }
+
+        int tempIndex = 1;
+        currentChar = new byte[charLength];
+        currentChar[0] = currentByte;
+        while(tempIndex < charLength){
+            currentChar[tempIndex++] = bFilArr[byteIndex++];
+        }
+        currentSequence.append(new String(currentChar));
+        return byteIndex;
     }
 
 
@@ -177,7 +185,7 @@ public class LZ {
      * @throws IOException the io exception
      */
     public void compress(String filePath, String outPath) throws IOException {
-        sequences = new StringBuffer();
+        StringBuffer sequences = new StringBuffer();
         DataInputStream innfil = new DataInputStream(new BufferedInputStream(new FileInputStream(filePath)));
         DataOutputStream utfil = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(outPath)));
         int prevIndex = -1;
@@ -185,7 +193,7 @@ public class LZ {
         StringBuilder currentSequence = new StringBuilder();
         byte[] currentOutputBlock = {};
 
-        bFilArr = new byte[innfil.available()];
+        byte[] bFilArr = new byte[innfil.available()];
         innfil.readFully(bFilArr, 0, innfil.available());
         int byteIndex = 0;
         boolean lastUncompressed = true;
@@ -248,38 +256,17 @@ public class LZ {
 
                     else{
                         for (int i = 3; i < currentOutputBlock.length; i++) {
-                            currentOutputBlock[i] = (byte) (currentChar[i-3] & 0xff);   //la til & 0xff
+                            currentOutputBlock[i] = (byte) (currentChar[i-3] & 0xff);
                         }
                     }
-                    //testing
 
-                    byte lastByte = 0;
-                    for (int i = 0; i < currentOutputBlock.length; i++) {
-                        if(lastByte == currentOutputBlock[i] && lastByte == -13){
-                            String binaryString = Integer.toBinaryString(currentByte & 0xff);
-                            System.out.println(binaryString);
-                            for (int j = 3; j < currentOutputBlock.length; j++) {
-                                System.out.println(Integer.toBinaryString(currentChar[j-3]& 0xff));
-                            }
-                        }
-                        else lastByte = currentOutputBlock[i];
-                    }
-
-
-                    if(currentOutputBlock[1] == -13){
-                        System.out.println("dette er umulig");
-                    }
-
-
-                    //testing
                     utfil.write(currentOutputBlock);
                 }else {
                     uncompressedOut.append(currentSequence.toString());
                     lastUncompressed = true;
                 }
-                //prevIndex = -1;
                 currentSequence = new StringBuilder();
-                if(sequences.length() > 1<<15) trimList();
+                if(sequences.length() > 1<<15) trimList(sequences);
             }else {
                 prevIndex = indexDos;
             }
@@ -335,12 +322,8 @@ public class LZ {
         utfil.write(currentOutputBlock);
     }
 
-
-
-    private void trimList(){
+    private void trimList(StringBuffer sequences){
         sequences.delete(0, sequences.length() - (1 << 15));
     }
-
-
 
 }
